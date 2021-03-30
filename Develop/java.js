c1834b8e76056e1b6735a5979a4025a542de3a4a
Joe Basquez <jybasquez@yahoo.com>
2021-03-29 22:20:51 -0400

    got future time by one hour

--- Develop/java.js
+++ Develop/java.js
@@ -3,4 +3,11 @@ var current = moment().format('llll');
 console.log(current);
 var hour = moment().format('LT');
 
-console.log(hour);
+var future = moment('2016-03-12 08:00:00').add(1, 'hours').format('LLL');
+console.log(future);
+
+// var a = moment('2016-01-01'); 
+// var b = a.clone().add(1, 'week'); 
+// a.format();
+// console.log(a);
+// console.log(b);

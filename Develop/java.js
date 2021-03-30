a8f6b2b368e24740f9e3c9933988a218b3dd72cf
Joe Basquez <jybasquez@yahoo.com>
2021-03-29 22:23:11 -0400

    added in past var

--- Develop/java.js
+++ Develop/java.js
@@ -6,6 +6,9 @@ var hour = moment().format('LT');
 var future = moment('2016-03-12 08:00:00').add(1, 'hours').format('LLL');
 console.log(future);
 
+var past = moment('2016-03-12 08:00:00').add(-1, 'hours').format('LLL');
+console.log(past);
+
 // var a = moment('2016-01-01'); 
 // var b = a.clone().add(1, 'week'); 
 // a.format();

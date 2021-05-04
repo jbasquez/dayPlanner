e147ef5594d436e6ede5704ba51b55236959a42c
Joe Basquez <jybasquez@yahoo.com>
2021-05-03 21:25:04 -0400

    current time rendering

--- Develop/java.js
+++ Develop/java.js
@@ -1,6 +1,6 @@
+var now = document.querySelector('.now');
 var setDate = document.querySelector('.now');
-var current = moment().format('llll'); 
-console.log(current);
+
 var hour = moment().format('LT');
 
 var future = moment('2016-03-12 08:00:00').add(1, 'hours').format('LLL');
@@ -14,3 +14,10 @@ console.log(past);
 // a.format();
 // console.log(a);
 // console.log(b);
+document.addEventListener("DOMContentLoaded", function(){
+     
+
+    now.innerHTML = moment().format('llll');
+
+}
+)

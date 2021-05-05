9483f4fd5ee0f7d1bd7725dbd8bf5877a86b1462
Joe Basquez <jybasquez@yahoo.com>
2021-05-05 17:42:03 -0400

    got proper hour from moment.js and set up structure for colorchanger function

--- Develop/java.js
+++ Develop/java.js
@@ -3,6 +3,15 @@ $( document ).ready(function() {
     //event listener for button to set and save id and text to local storage
     $( ".saveBtn" ).click(function() {
         alert( "Handler for .click() called." );
-      
       });
+      function colorChanger() {
+        let hour = moment().hours();
+        console.log(hour);
+        //loop through time blocks
+
+
+        // https://api.jquery.com/jquery.each/
+
+      }
+      colorChanger();
 });

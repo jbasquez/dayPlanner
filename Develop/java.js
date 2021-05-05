2b8fb0c2c492dbb13ec05c41a78fb8a88d2e522f
Joe Basquez <jybasquez@yahoo.com>
2021-05-04 20:29:53 -0400

    got button to return on click

--- Develop/java.js
+++ Develop/java.js
@@ -1,23 +1,30 @@
-var now = document.querySelector('.now');
-var setDate = document.querySelector('.now');
+// var now = document.querySelector('.now');
+// var setDate = document.querySelector('.now');
 
-var hour = moment().format('LT');
+// var hour = moment().format('LT');
 
-var future = moment('2016-03-12 08:00:00').add(1, 'hours').format('LLL');
-console.log(future);
+// var future = moment('2016-03-12 08:00:00').add(1, 'hours').format('LLL');
+// console.log(future);
 
-var past = moment('2016-03-12 08:00:00').add(-1, 'hours').format('LLL');
-console.log(past);
+// var past = moment('2016-03-12 08:00:00').add(-1, 'hours').format('LLL');
+// console.log(past);
 
-// var a = moment('2016-01-01'); 
-// var b = a.clone().add(1, 'week'); 
-// a.format();
-// console.log(a);
-// console.log(b);
-document.addEventListener("DOMContentLoaded", function(){
+// // var a = moment('2016-01-01'); 
+// // var b = a.clone().add(1, 'week'); 
+// // a.format();
+// // console.log(a);
+// // console.log(b);
+// document.addEventListener("DOMContentLoaded", function(){
      
 
-    now.innerHTML = moment().format('llll');
+//     now.innerHTML = moment().format('llll');
 
-}
-)
+// }
+// )
+$( document ).ready(function() {
+    console.log( "ready!" );
+    //event listener for button to set and save id and text to local storage
+    $( ".saveBtn" ).click(function() {
+        alert( "Handler for .click() called." );
+      });
+});

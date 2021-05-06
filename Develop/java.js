75fd666821eb383fd764a89af1b784b497acf051
Joe Basquez <jybasquez@yahoo.com>
2021-05-05 21:19:08 -0400

    added in some comments small changes

--- Develop/java.js
+++ Develop/java.js
@@ -1,17 +1,18 @@
+//tells document to ready once fully loaded
 $( document ).ready(function() {
+    //console.log to verify its running
     console.log( "ready!" );
     //event listener for button to set and save id and text to local storage
     $( ".saveBtn" ).click(function() {
         alert( "Handler for .click() called." );
+         //save 2 variables 
+         var toDoHour;
+         var singleToDo;
+         // $(this) form
+         console.log(this);
+
+         // set to local storage
+         //localStorage.setItem(singleToDo,toDoHour)
       });
-      function colorChanger() {
-        let hour = moment().hours();
-        console.log(hour);
-        //loop through time blocks
-
-
-        // https://api.jquery.com/jquery.each/
-
-      }
-      colorChanger();
+      
 });

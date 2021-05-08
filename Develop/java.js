d289fbb232fba67a876378301bc7fb4d809c7c5e
Joe Basquez <jybasquez@yahoo.com>
2021-05-08 13:57:07 -0400

    added current date and time to html page

--- Develop/java.js
+++ Develop/java.js
@@ -2,18 +2,20 @@
 $( document ).ready(function() {
     //console.log to verify its running
     console.log( "ready!" );
+    //calls on the class current time and adds the current time to the empty field when the page is fully loaded
+    currentDay.innerHTML = moment().format('llll');
     //event listener for button to set and save id and text to local storage
     $( ".saveBtn" ).click(function() {
         alert( "Handler for .click() called." );
          //save 2 variables 
-         var toDoHour;
-         //var singleToDo=$(this).siblings(".toDo")[0].value;
+         var toDoHour=$(this).siblings(".toDo").attr("id");
+         var singleToDo=$(this).siblings(".toDo")[0].value;
          // calling variables from index.html
-         console.log($(this).siblings(".toDo")[0].value);
-         console.log($(this).siblings(".toDo").attr("id"));
+         console.log(toDoHour);
+         console.log(singleToDo);
 
          // set/save to local storage
-         //localStorage.setItem(singleToDo,toDoHour)
+         localStorage.setItem(singleToDo,toDoHour)
       });
       
 });
